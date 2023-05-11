c3c98a40255adfa5b1eb30532fbe689d8d2bd29b
IbtissamNoukta <ibtissam.noukta@gmail.com>
2023-05-11 20:34:34 +0100

    Add photo to contacts

--- app/Http/Livewire/Contacts.php
+++ app/Http/Livewire/Contacts.php
@@ -3,18 +3,22 @@
 namespace App\Http\Livewire;
 use App\Models\Contact;
 use Livewire\WithPagination;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 
 use Livewire\Component;
 
 class Contacts extends Component{
     use WithPagination;
-
+    use WithFileUploads;
 
     public $contacts;
     public $name;
     public $phone;
     public $contactId;
+    public $photo;
     public $editing = false;
 
     protected $rules = [
@@ -42,11 +46,18 @@ class Contacts extends Component{
         $newContact = new Contact();
         $newContact->name = $this->name;
         $newContact->phone = $this->phone;
+        if($this->photo){
+            $this->validate([
+                'photo' => 'image|max:1024',
+            ]);
+            $newContact->photo = $this->storePhoto();
+        }
         $newContact->save();
         //The prepend method will add a a given $value to the beginning of the collection
         $this->contacts->prepend($newContact);
         $this->name='';
         $this->phone='';
+        $this->photo='';
         session()->flash('message', 'Contact successfully added.');
     }
     public function getContact($contactId){
@@ -54,12 +65,14 @@ class Contacts extends Component{
         $this->contactId = $contact->id;
         $this->name = $contact->name;
         $this->phone = $contact->phone;
+        $this->photo = $contact->photo;
         $this->editing = true;
     }
     public function cancelUpdate(){
         $this->contactId = '';
         $this->name = '';
         $this->phone = '';
+        $this->photo='';
         $this->editing = false;
     }
     public function updateContact(){
@@ -67,30 +80,50 @@ class Contacts extends Component{
         $contact = Contact::find($this->contactId);
         $contact->name = $this->name;
         $contact->phone = $this->phone;
+        if($this->photo != $contact->photo){
+            if(File::exists(public_path('./storage/photos/'.$contact->photo))){
+                File::delete(public_path('./storage/photos/'.$contact->photo));
+            }
+            if($this->photo){
+                $this->validate([
+                    'photo' => 'image|max:1024',
+                ]);
+                $contact->photo = $this->storePhoto();
+            }
+        }
         $contact->update();
         $this->getContacts();
         $this->contactId = '';
         $this->name = '';
         $this->phone = '';
+        $this->photo='';
         $this->editing = false;
         session()->flash('message', 'Contact successfully updated.');
 
     }
     public function deleteContact($contactId){
         $contact = Contact::find($contactId);
+        if(File::exists(public_path('./storage/photos/'.$contact->photo))){
+            File::delete(public_path('./storage/photos/'.$contact->photo));
+        }
         $contact->delete();
         $this->getContacts();
         $this->contactId = '';
         $this->name = '';
         $this->phone = '';
+        $this->photo='';
         $this->editing = false;
         session()->flash('message', 'Contact successfully deleted.');
 
+    }
+    public function storePhoto(){
+        $this->photo->storeAs('photos', $this->photo->getClientOriginalName(), 'public');
+        return $this->photo->getClientOriginalName();
     }
 
     public function render()
     {
         // ['name' => $this->name]
-        return view('livewire.contacts')->with(['contactsList' => Contact::paginate(3)]);
+        return view('livewire.contacts')->with(['contactsList' => Contact::latest()->paginate(3)]);
     }
 }
--- resources/views/livewire/contacts.blade.php
+++ resources/views/livewire/contacts.blade.php
@@ -2,6 +2,23 @@
     {{-- The whole world belongs to you. --}}
     <div class="relative mx-auto w-auto max-w-2xl">
         <div class="bg-white w-full mt-2 p-5 border-solid border-2 rounded-md divide-gray-400">
+            <label class="block mb-4">
+                <span class="block text-sm font-medium text-slate-700">Contact image</span>
+                <!-- Using form state modifiers, the classes can be identical for every input -->
+                <input wire:loading wire:model="photo" type="file" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"/>
+                @error('image') <span class="error text-red-600">{{ $message }}</span> @enderror
+            </label>
+            <label class="block my-2">
+                @if ($photo)
+                Photo Preview:
+                    @if(is_string($photo))
+                        <img width="200" src="{{ asset("storage/photos/".$photo) }}">
+                    @else
+                        <img width="200" src="{{ $photo->temporaryUrl() }}">
+                    @endif
+                @endif
+            </label>
             <label class="block my-4">
                 <span class="block text-sm font-medium text-slate-700">Contact name</span>
                 <!-- Using form state modifiers, the classes can be identical for every input -->
@@ -49,7 +66,7 @@
             <li class="justify-between gap-x-6 py-5">
         <div class="flex gap-x-4">
             <!-- :src="person.imageUrl" -->
-            <img class="h-12 w-12 flex-none rounded-full bg-gray-50"  alt="" />
+            <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="{{ asset("storage/photos/".$contact->photo) }}"/>
             <div class="min-w-0 flex-auto">
             <p class="text-sm font-semibold leading-6 text-gray-900">{{ $contact->name }}</p>
             <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ $contact->phone  }}</p>
@@ -66,7 +83,7 @@
         </li>
         @endforeach
     </ul>
-    <div>
+    <div class="mb-2">
         {{ $contactsList->links('custom-pagination-links-view') }}
     </div>
 </div>
